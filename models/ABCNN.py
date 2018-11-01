d793de586b12102a99957556baa969c1923690c0
speng <speng@freewheel.tv>
2018-11-01 20:11:19 +0800

    update

--- models/ABCNN.py
+++ models/ABCNN.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 '''
  implement of the paper: "ABCNN: Attention-Based Convolutional Neural Network for Modeling Sentence Pairs"
 '''
+
+
 class ABCNN3(nn.Module):
     def __init__(self, args):
         super(ABCNN3, self).__init__()
@@ -28,10 +30,11 @@ class ABCNN3(nn.Module):
 
     def forward(self, *input):
         s1, s2 = input[0], input[1]
-        mask1, mask2 = s1.eq(constant.PAD_INDEX), s2.eq(constant.PAD_INDEX)
+        mask1, mask2 = s1.eq(0), s2.eq(0)
         res = [[], []]
         s1, s2 = self.embeds(s1), self.embeds(s2)
-        batch_size, seq_len, dim = s1.shape
+        # eg: s1 => res[0]
+        # (batch_size, seq_len, dim) => (batch_size, dim)
         res[0].append(F.max_pool1d(s1.transpose(1, 2), kernel_size=s1.size(1)).squeeze(-1))
         res[1].append(F.max_pool1d(s2.transpose(1, 2), kernel_size=s2.size(1)).squeeze(-1))
         for i, conv in enumerate(self.conv):
@@ -40,11 +43,11 @@ class ABCNN3(nn.Module):
             res[1].append(F.max_pool1d(o2.transpose(1, 2), kernel_size=o2.size(1)).squeeze(-1))
             o1, o2 = attention_avg_pooling(o1, o2, mask1, mask2)
             s1, s2 = o1 + s1, o2 + s2
-        
+        # batch_size * (dim*(1+num_layer)*2) => batch_size * linear_size
         x = torch.cat([torch.cat(res[0], 1), torch.cat(res[1], 1)], 1)
-        # import ipdb; ipdb.set_trace()
         sim = self.fc(x)
         return sim
+
 
 class Wide_conv(nn.Module):
     def __init__(self, seq_len, embeds_size):
@@ -53,11 +56,7 @@ class Wide_conv(nn.Module):
         self.embeds_size = embeds_size
         self.W = nn.Parameter(torch.randn([seq_len, embeds_size]))
         nn.init.xavier_normal_(self.W)
-        self.conv = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=[3,3], padding=[1, 1], stride=1)
-        self.fc = nn.Sequential(
-            # Swish(),
-        )
-
+        self.conv = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=3, padding=[1, 1], stride=1)
         
     def forward(self, sent1, sent2, mask1, mask2):
         '''
@@ -65,21 +64,24 @@ class Wide_conv(nn.Module):
         return 
         '''
         # sent1, sent2 = sent1.transpose(0, 1), sent2.transpose(0, 1)
-        A = match_score(sent1, sent2, mask1, mask2) # A: batch_size * seq_len * seq_len
-        
-        attn_feature_map1 = A.matmul(self.W) # attn_feature_map1: batch_size * seq_len * dim
+        # => A: batch_size * seq_len * seq_len
+        A = match_score(sent1, sent2, mask1, mask2)
+        # attn_feature_map1: batch_size * seq_len * dim
+        attn_feature_map1 = A.matmul(self.W)
         attn_feature_map2 = A.transpose(1, 2).matmul(self.W)
-        x1 = torch.cat([sent1.unsqueeze(1), attn_feature_map1.unsqueeze(1)], 1) # x1: batch_size * 2 *seq_len * dim
+        # x1: batch_size * 2 *seq_len * dim
+        x1 = torch.cat([sent1.unsqueeze(1), attn_feature_map1.unsqueeze(1)], 1)
         x2 = torch.cat([sent2.unsqueeze(1), attn_feature_map2.unsqueeze(1)], 1)
         o1, o2 = self.conv(x1).squeeze(1), self.conv(x2).squeeze(1)
-        return self.fc(o1), self.fc(o2)
+        return o1, o2
 
 
 def attention_avg_pooling(sent1, sent2, mask1, mask2):
     '''
     sent1, sent2: (batch_size, seq_len, dim)
     '''
-    A = match_score(sent1, sent2, mask1, mask2)# A: batch_size * seq_len * seq_len
+    # A: batch_size * seq_len * seq_len
+    A = match_score(sent1, sent2, mask1, mask2)
     weight1 = torch.sum(A, -1)
     weight2 = torch.sum(A.transpose(1, 2), -1)
     s1 = sent1 * weight1.unsqueeze(2)
